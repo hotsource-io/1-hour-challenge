064b715e4fd85160fc8fb70b3b60678185c3b891
matthew@minion.co.nz <matthew@minion.co.nz>
2019-02-19 14:58:38 +1300

    CSetup challenge environment

---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Component from '../src';
+
+storiesOf('One Hour Challenge', module)
+  .add('basic example', () => (
+    <Component></Component>
+  ));
